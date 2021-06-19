1290d49bd0c2437fbbf9a2379b9859eafd63290e
Mateus Canelhas <mateus.canelhas@gmail.com>
2021-06-19 17:19:21 -0300

    Added deploy workflows

--- src/main/framework/objects/stores.ts
+++ src/main/framework/objects/stores.ts
@@ -1,25 +1,19 @@
 import type { Readable, Writable } from "svelte/store";
 import { get } from "svelte/store";
 import { mutate } from "../../categories/morphisms/functional";
-import fastdom from "fastdom";
 
 export type Store<T> = Readable<T> | Writable<T>
 
 
 export function updateStore<T>( store: Writable<T>, state: T ): void {
 
-	fastdom.mutate(
-		() => {
-			store?.set( mutate( readStore( store ),
-								state ) )
-		}
-	)
+	store?.set( mutate( readStore( store ),
+								state ))
 
 }
 
 export function readStore<T>( store: Writable<T> ): T {
 
-	return fastdom.measure(
-		() => get( store )
-	)()
+
+	return get( store )
 }
