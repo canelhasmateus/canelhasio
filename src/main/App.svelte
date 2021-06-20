6dc89b2a0a5890eb6e657e6b023ef163980a302c
Mateus Canelhas <mateus.canelhas@gmail.com>
2021-06-19 21:04:07 -0300

    Temporary meme.

--- src/main/App.svelte
+++ src/main/App.svelte
@@ -1,125 +1,91 @@
-
-
-<script lang='ts'>
-
+<script lang="ts">
 	import type { Writable } from "svelte/store";
 	import { get, writable } from "svelte/store";
 	import type { Fade, Place, Shape } from "./categories/objects/measurements";
-	import { defaultFadeIn, defaultFadeOut, defaultPosition, defaultShapeIn, defaultShapeOut, toPercent, toPixels, toTranslate } from "./framework/objects/properties";
+	import {
+		defaultFadeIn,
+		defaultFadeOut,
+		defaultPosition,
+		defaultShapeIn,
+		defaultShapeOut,
+		toPercent,
+		toPixels,
+		toTranslate,
+	} from "./framework/objects/properties";
 	import { updateStore } from "./framework/objects/stores";
 	import { mapValues } from "./categories/morphisms/javascript";
-	import Canvas from "./components/Canvas.svelte"
+	import Canvas from "./components/Canvas.svelte";
 	import { spring } from "svelte/motion";
 
+	const hoverTransparency: Writable<Fade> = writable(defaultFadeOut);
 
-	const hoverTransparency: Writable<Fade> = writable( defaultFadeOut )
-	const hoverCoordinates: Writable<Place> = spring( defaultPosition )
-	const hoverSize: Writable<Shape>        = writable( defaultShapeIn )
+	const hoverCoordinates: Writable<Place> = spring(defaultPosition);
+	const hoverSize: Writable<Shape> = writable(defaultShapeIn);
 
-	$: style = toTranslate( $hoverCoordinates )
-	function fillHover( event: MouseEvent ) {
+	$: style = toTranslate($hoverCoordinates);
 
-		updateStore( hoverTransparency,
-					 mapValues( toPercent,
-								defaultFadeIn ) )
+	function fillHover(event: MouseEvent) {
+		updateStore(hoverTransparency, mapValues(toPercent, defaultFadeIn));
 	}
 
-	function fadeHover( event: MouseEvent ) {
-		updateStore( hoverTransparency,
-					 mapValues( toPercent,
-								defaultFadeOut ) )
+	function fadeHover(event: MouseEvent) {
+		updateStore(hoverTransparency, mapValues(toPercent, defaultFadeOut));
 	}
 
-	function increaseHover( event: MouseEvent ) {
-		updateStore( hoverSize,
-					 mapValues( toPixels,
-								defaultShapeOut )
-		)
-
+	function increaseHover(event: MouseEvent) {
+		updateStore(hoverSize, mapValues(toPixels, defaultShapeOut));
 	}
 
-	function decreaseHover( event: MouseEvent ) {
-		updateStore( hoverSize,
-					 mapValues( toPixels,
-								defaultShapeIn )
-		)
+	function decreaseHover(event: MouseEvent) {
+		updateStore(hoverSize, mapValues(toPixels, defaultShapeIn));
 	}
 
-	function displaceHover( event: MouseEvent ) {
-
-		updateStore( hoverCoordinates,
-					 {
-						 x: event.x,
-						 y: event.y
-					 }
-		)
-
+	function displaceHover(event: MouseEvent) {
+		updateStore(hoverCoordinates, {
+			x: event.x,
+			y: event.y,
+		});
 	}
-
 
 </script>
 
-<main
-		class="flex-row flex-center">
-
+<main class="flex-row flex-center">
 	<div
-			class="flex-row flex-center"
-			on:mouseenter={ fillHover }
-			on:mouseleave={ fadeHover }
-			on:mousedown={ increaseHover }
-			on:mouseup={ decreaseHover }
-			on:mousemove={ displaceHover }
+		class="flex-row flex-center"
+		on:mouseenter={fillHover}
+		on:mouseleave={fadeHover}
+		on:mousedown={increaseHover}
+		on:mouseup={decreaseHover}
+		on:mousemove={displaceHover}
 	>
-
-		<h1>Coming Soon&trade</h1>
+		<h1>Luana é Linda</h1>
 
 		<Canvas>
-			<circle
-					{...$hoverTransparency}
-					{...$hoverSize}
-					style={style}
-			>
-
-			</circle>
+			<circle {...$hoverTransparency} {...$hoverSize} {style} />
 		</Canvas>
-
 	</div>
-
 </main>
 
-
 <style>
-
 	h1 {
-
 		font-size: 10em;
 		font-weight: 700;
 		font-family: "Baloo Da 2", monospace;
 		font-style: normal;
-
 	}
 
-
-
-
-
-
-
-
-
-
-
 	@font-face {
-		font-family: 'Baloo Da 2';
+		font-family: "Baloo Da 2";
 		font-style: normal;
 		font-weight: 700;
 		font-display: fallback;
-		src: url(/assets/balooda2.woff2) format('woff2');
-		unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+		src: url(/assets/balooda2.woff2) format("woff2");
+		unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6,
+			U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193,
+			U+2212, U+2215, U+FEFF, U+FFFD;
 	}
 
 	:global(html > body) {
-
 		height: 100vh;
 		overflow: hidden;
 
@@ -128,7 +94,6 @@
 
 		color: rgb(197, 79, 10);
 		background-color: #1a1618;
-
 	}
 
 	circle {
@@ -137,40 +102,30 @@
 		transition-duration: 300ms;
 		position: absolute;
 		border-radius: 50%;
-		fill: rgb(197, 79, 10) ;
+		fill: rgb(197, 79, 10);
 	}
 
-
 	.flex-col {
-
 		height: 100%;
 		width: 100%;
 		display: flex;
 		flex-direction: column;
 		flex-grow: 1;
 		flex-shrink: 0;
-
 	}
 
 	.flex-row {
-
 		height: 100%;
 		width: 100%;
 		display: flex;
 		flex-direction: row;
 		flex-grow: 1;
 		flex-shrink: 0;
-
 	}
 
 	.flex-center {
-
 		justify-content: center;
 		align-items: center;
-
 	}
 
-
 </style>
-
-
--- src/main/framework/objects/stores.ts
+++ src/main/framework/objects/stores.ts
@@ -1,19 +1,13 @@
-import type { Readable, Writable } from "svelte/store";
-import { get } from "svelte/store";
-import { mutate } from "../../categories/morphisms/functional";
+import type { Readable, Writable } from "svelte/store"
+import { get } from "svelte/store"
+import { mutate } from "../../categories/morphisms/functional"
 
 export type Store<T> = Readable<T> | Writable<T>
 
-
-export function updateStore<T>( store: Writable<T>, state: T ): void {
-
-	store?.set( mutate( readStore( store ),
-								state ))
-
+export function updateStore<T> ( store: Writable<T>, state: T ): void {
+  store?.set( mutate( readStore( store ), state ) )
 }
 
-export function readStore<T>( store: Writable<T> ): T {
-
-
-	return get( store )
+export function readStore<T> ( store: Writable<T> ): T {
+  return get( store )
 }
