dacb785c649a4fad78048e0b6108c1acd3de525a
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-06-02 13:55:08 -0300

    $Message

--- src/main/categories/objects/numericals.ts
+++
@@ -1,8 +0,0 @@
-export type Numerical = number
-export type Positive = () => Numerical
-export type Bounded<Low = any, High = Low> = ( low, high ) => Bounded<typeof low, typeof high>
-
-
-export type Value<T> = ( T ) => Value<T>
-export type Zero<T> = ( T ) => Zero<T>
-export type Infinite<T> = ( T ) => Infinite<T>
--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,20 @@
-const sveltePreprocess = require('svelte-preprocess')
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
 
-module.exports = {
-    // Consult https://github.com/sveltejs/svelte-preprocess
-    // for more information about preprocessors
-    preprocess: sveltePreprocess()
-}
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: adapter(),
+
+		// Override http methods in the Todo forms
+		methodOverride: {
+			allowed: ['PATCH', 'DELETE']
+		}
+	}
+};
+
+export default config;
