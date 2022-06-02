595297f476c515b5d03720c8ce1171e08785fbee
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-06-02 13:55:08 -0300

    Switched to sveltekit.

--- src/main/categories/objects/functors.ts
+++
@@ -1,3 +0,0 @@
-export type Finish = () => void;
-export type Functor<K = any, V = any> = ( arg: K ) => V
-export type Composition<K, V> = Functor<Functor<K, any>, V>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,15 @@
+<nav>
+    <a href="/">Home</a>
+    <a href="/projects">Projects</a>
+    <a href="/blog">Blog</a>
+    <a href="/drafts">Drafts</a>
+</nav>
+
+<slot>
+
+</slot>
+
+
+<footer>
+    Footer
+</footer>
